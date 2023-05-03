46bd929b2ff298a1e4c3b3968a37bacebe669304
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-03 21:04:57 +0530

    Update and rename Gross Pay to Program.c

--- Program.c
+++ Program.c
@@ -1,4 +1,9 @@
-/* Gross Pay */
+Ramesh's basic salary is input through the keyboard. 
+His dearness allowance is 40% of basic salary, and house rent allowance is 20% of basic salary.
+Write a program to calculate his gross salary.
+
+
+
 #include<stdio.h>
 #include<conio.h>
 int main ()
